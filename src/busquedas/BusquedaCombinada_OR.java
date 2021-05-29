90eefc322758fad3d184121e0c007042ad6eb3de
cattsas <cattsas1980@gmail.com>
2021-05-29 15:56:19 -0300

    Se implementa clase BusquedaCombinada_OR

---
+++ src/busquedas/BusquedaCombinada_OR.java
@@ -0,0 +1,18 @@
+package src.busquedas;
+
+import src.ElementoComida;
+
+public class BusquedaCombinada_OR implements CriterioBusqueda{
+    CriterioBusqueda c1;
+    CriterioBusqueda c2;
+
+    public BusquedaCombinada_OR(CriterioBusqueda c1, CriterioBusqueda c2) {
+        this.c1 = c1;
+        this.c2 = c2;
+    }
+
+    @Override
+    public boolean cumple(ElementoComida comida) {
+        return(c1.cumple(comida) || c2.cumple(comida));
+    }
+}
